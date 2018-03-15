8f7b8e2be0fd114fb6bbcb253ed87d1db2319e2c
Murthy10 <samuel.kurath@gmail.com>
2018-03-15 19:14:30 +0100

    updated random player

--- pytictactoe/player/random_player.py
+++ pytictactoe/player/random_player.py
@@ -1,6 +1,5 @@
 import random
 
-from pytictactoe.field import Field
 from pytictactoe.player.base_player import BasePlayer
 
 
@@ -8,8 +7,8 @@ class RandomPlayer(BasePlayer):
     def choose_field(self, grid):
         allowed = False
         empty_fields = grid.get_empty_fields()
+        field = empty_fields[random.randint(0, len(empty_fields) - 1)]
         while not allowed:
-            field = Field(x=random.randint(0, 2), y=random.randint(0, 2))
             allowed = yield field
             if allowed:
                 yield None
